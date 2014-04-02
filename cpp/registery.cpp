1391fa86137665898478a8275bbeccd3f9a67368
dariush <b.g.dariush@gmail.com>
2014-04-03 02:41:36 +0430

    + registery

---
+++ cpp/registery.cpp
@@ -0,0 +1,13 @@
+/* 
+ * File:   registery.cpp
+ * Author: dariush
+ * 
+ * Created on April 3, 2014, 1:22 AM
+ */
+#include "../hpp/registery.hpp"
+
+NS CPP_TESTER {
+#if defined(__TESTING__) && !defined(__DEMO__)
+    test_suite registery::__suite;
+#endif
+}
