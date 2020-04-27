992f02e5d473228aae353ac6166fef3c939115b4
Dariush Hasapour <d.hasanpour@virtux.nl>
2020-04-27 17:34:16 +0430

    adding events sample to the `sample tester`

--- tests/sample_tester.hpp
+++ tests/sample_tester.hpp
@@ -1,9 +1,34 @@
-#ifndef SAMPLE_TESTER_HPP
-#define SAMPLE_TESTER_HPP
+#pragma once
 
+#include <iostream>
 #include "lizard/lizard.hpp"
 
 TESTER(sample_tester,
+
+    // onbefore([]() {
+    //     std::cout << std::endl << "BEFORE ANY SPEC GOT STARTED!" << std::endl;
+    // });
+
+    // onafter([]() {
+    //     std::cout << std::endl << "AFETR ALL SPECs GOT FINISHED!" << std::endl;
+    // });
+
+    // onskip([](auto name, auto err) {
+    //     std::cout << std::endl << "SKIPPED: " << name << " WITH MESSAGE: " << err.what() << std::endl;
+    // });
+
+    // onfail([](auto name, auto err) {
+    //     std::cout << std::endl << "FAILED: " << name << " WITH MESSAGE: " << err.what() << std::endl;
+    // });
+
+    // onprespec([](auto name) {
+    //     std::cout << std::endl << "STARTING SPEC: " << name << std::endl;
+    // });
+
+    // onpostspec([](auto name) {
+    //     std::cout << std::endl << "FINISHED SPEC: " << name << std::endl;
+    // });
+
     spec(sum, []() {
         IS_EQUAL(1 + 2, 3);
     });
@@ -24,5 +49,3 @@ TESTER(sample_tester,
         IS_EQUAL(2 / 2, 1);
     });
 )
-
-#endif
