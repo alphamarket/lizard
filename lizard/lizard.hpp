84b9d8c30b6eb58cdaaea4a98df64e8b64bb6a2e
Dariush Hasapour <d.hasanpour@virtux.nl>
2020-04-27 17:32:34 +0430

    some improvements for `lizard.hpp`

--- lizard/lizard.hpp
+++ lizard/lizard.hpp
@@ -1,65 +1,14 @@
-#ifndef LIZARD_HPP
-#define	LIZARD_HPP
+#pragma once
 
 #define LIZARD_VERSION "2.0.2"
 
-#ifdef __unix__
-#   define OS_UNIX
-#else
-#   define OS_WIN
-#   warning "The compilation of this source in any other OS than UNIX may cause many errors!"
-#endif
-
-#ifdef __GNUC__
-#   define __unused __attribute__((unused))
-#else
-#   define __unused
-#endif
-
 #include <string>
-#include <time.h>
-#include <random>
 #include <vector>
-#include <memory>
 #include <sstream>
-#include <cstdlib>
-#include <stdlib.h>
-#include <assert.h>
-#include <iostream>
-#include <stdexcept>
-
-#define get_rand(min, range)((min) + rand() % (range))
-#define frand()             (double(rand()) / RAND_MAX)
-#ifdef OS_UNIX
-#   include <unistd.h>
-#   define dynamic_seed     (time(NULL) + rand() + getpid() * 100)
-#else
-#   define dynamic_seed     (time(NULL) + rand())
-#endif
-#define updateseed()        srand(dynamic_seed)
-
-#ifdef __GNUC__
-#	define __deprecated(func) func __attribute__ ((deprecated))
-#elif defined(_MSC_VER)
-#	define __deprecated(func) __declspec(deprecated) func
-#else
-#	pragma message("WARNING: You need to implement `__deprecated` for this compiler!")
-#	define __deprecated(func) func
-#endif
-
-#ifdef OS_UNIX
-#   define clear_screen() system("clear")
-#else
-#   ifdef OS_WIN
-#       define clear_screen() system("cls")
-#   else
-#       define clear_screen()
-#   endif
-#endif
-
-#include <exception>
 #include <assert.h>
 #include <typeinfo>
+#include <exception>
+#include <unordered_map>
 #if __cplusplus >= 201103L
 #   include <type_traits>
 #endif
@@ -426,5 +375,3 @@ namespace lizard {
 #define $(label, name) lizard_register_custom(label, name)
 
 #define MANIFEST(...) namespace lizard { void lizard_manifest () { __VA_ARGS__ } }
-
-#endif	/* LIZARD_HPP */
